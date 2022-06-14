81b5835ca7355129ecdeaef02f3b6a5bc4729998
adrey1975 <reyesadrian34@gmail.com>
2022-06-14 18:13:39 -0500

    added function to append text on site depending which npc is generated

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+// Array holding our images.
 var myPix = new Array ("images/easy-pete.jpg", "images/patches.jpg");
 
+// Function that will select random images from our array.
 function choosePic() {
     var randomNum = Math.floor(Math.random() * myPix.length);
     document.getElementById("npcs").src = myPix[randomNum];
+}
+
+// Function to change/append h3 text depending on images generated
+function addText() {
+    if (myPix = new Array[0]) {
+        $("#text").append("Easy Pete from Fallout New Vegas");
+    } else {
+        $("#text").append("Patches");
+    }
+
 }
