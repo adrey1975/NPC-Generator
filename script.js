93c89c1e081d14885e9f19ef07bfa6b8e25544ed
adrey1975 <reyesadrian34@gmail.com>
2022-06-14 19:08:09 -0500

    removed changeText function and included if else statement in choosePic function instead

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 // Array holding our images.
 var myPix = new Array ("images/easy-pete.jpg", "images/patches.jpg");
-
+ 
 // Function that will select random images from our array.
 function choosePic() {
     var randomNum = Math.floor(Math.random() * myPix.length);
     document.getElementById("npcs").src = myPix[randomNum];
-}
-
-// Function to change/append h3 text depending on images generated
-function addText() {
-    if (myPix = new Array[0]) {
-        $("#text").append("Easy Pete from Fallout New Vegas");
+    
+    if(myPix[randomNum] === "images/easy-pete.jpg") {
+        alert("Easy Pete!")
     } else {
-        $("#text").append("Patches");
+        alert("Patches!")
     }
 
 }
